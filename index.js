759134d84b58a55b742da22e6f64f3bc0bfd76e4
as-you-say <dgf5820@daum.net>
2020-07-27 03:02:59 +0900

    # 수정 - 로그인 기능 추가

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 // 데이터베이스 관련 모듈
 const { db } = require('./database');
@@ -9,8 +10,10 @@ const { User } = require('./models/User');
 // 미들웨어
 // application/x-www-form-urlencoded
 // application/json
+// cookie
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // 라우터
 app.get('/', (req, res) => res.send('안녕하세요~'));
@@ -22,6 +25,34 @@ app.post('/register', (req, res) => {
     if (err) return res.json({ success: false, err })
     return res.status(200).json({
       success: true
+    })
+  });
+});
+app.post('/login', (req, res) => {
+  // 1. 요청한 이메일이 데이터베이스에 있는지 찾는다.
+  User.findOne({ emain: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '제공된 이메일에 해당하는 유저가 없습니다.'
+      })
+    }
+
+    // 2. 요청된 이메일이 데이터베이스에 있다면, 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.' })
+      // 3. 비밀번호까지 맞다면, 토큰을 생성하기
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err)
+
+        // 4. 토큰을 저장한다. -- 어디에? : 쿠키, 로컬스토리지, 세선
+        // 어디에 저장해야 안전하냐는 부분은 논란이 많다 ... 각자 다 장단점이 있습니다.
+        // 이번에는 쿠키를 사용한다.
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +33,47 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+// 비밀번호 암호화
+userSchema.pre('save', (next) => {
+  var user = this;
+  if (user.isModified('password')) {
+    // - salt 생성 (saltRounds 는 salt의 자릿수를 말한다.)
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err)
+      
+      // - salt 를 이용해서 비밀번호를 암호화
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err) 
+
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err)
+    cb(null, isMatch)
+  });
+}
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  
+  // jsonwebtoken 을 이용해서 토큰을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save((err, user) => {
+    if(err) return cb(err)
+    cb(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
